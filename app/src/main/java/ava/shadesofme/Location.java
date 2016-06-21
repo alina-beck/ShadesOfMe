72803105ef34a955582d0e74f6ca879a969cde81
AndroidAva <4ndroid4va@gmail.com>
2016-06-21 09:08:52 +0200

    added locations

--- app/src/main/java/ava/shadesofme/Location.java
+++ app/src/main/java/ava/shadesofme/Location.java
@@ -1,4 +1,20 @@
 package ava.shadesofme;
 
+import java.util.ArrayList;
+
 public class Location {
+
+    private ArrayList<Item> items;
+    private GameStateManager gameStateManager;
+
+    public Location(ArrayList<Item> items, GameStateManager gameStateManager) {
+        this.items = items;
+        this.gameStateManager = gameStateManager;
+    }
+
+    public ArrayList<Item> search() {
+        int searchTime = 30 + (items.size() * 5);
+        gameStateManager.advanceBy(searchTime);
+        return items;
+    }
 }
--- app/src/test/java/ava/shadesofme/LocationTest.java
+++ app/src/test/java/ava/shadesofme/LocationTest.java
@@ -1,7 +1,43 @@
 package ava.shadesofme;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
 
 public class LocationTest {
 
+    private Location location;
+    private ArrayList<Item> items = new ArrayList<>();
+    private GameStateManager gameStateManager = Mockito.mock(GameStateManager.class);
+
+    @Before
+    public void setUp() {
+        items.add(Mockito.mock(Item.class));
+        items.add(Mockito.mock(Item.class));
+        items.add(Mockito.mock(Item.class));
+        this.location = new Location(items, gameStateManager);
+    }
+
+    @Test
+    public void whenSearchedLocationReturnsListOfItems() {
+        assertEquals(items, location.search());
+    }
+
+    @Test
+    public void whenSearchedLocationTellsGameStateManagerToAdvanceTime() {
+        location.search();
+        verify(gameStateManager).advanceBy(45);
+    }
+
+    @Test
+    public void searchTimeIsCalculatedFromNumberOfItems() {
+        items.add(Mockito.mock(Item.class));
+        location.search();
+        verify(gameStateManager).advanceBy(50);
+    }
 }
