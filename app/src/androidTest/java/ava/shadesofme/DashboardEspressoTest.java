adfec657cd9066b4a4b7512a9825e464c4bd4cae
AndroidAva <4ndroid4va@gmail.com>
2016-07-08 10:39:28 +0200

    getting Espresso tests to run reliably on Genymotion

--- app/src/androidTest/java/ava/shadesofme/DashboardEspressoTest.java
+++ app/src/androidTest/java/ava/shadesofme/DashboardEspressoTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -18,49 +19,45 @@ public class DashboardEspressoTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    // TODO: mock Intent to verify that contents are distributed to views correctly
+
+    /**
+     * Activity is not destroyed after each test, resulting in AmbiguousViewMatcherException for random tests.
+     * This is a known issue and will be addressed in the next release - wait for it.
+     * TODO: update Espresso version to make tests reliable?
+     */
 
     @Test
-    public void currentLocationIsDisplayed() {
+    public void currentLocationAndTimeAreDisplayed() {
         onView(withId(R.id.text_location)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void currentTimeIsDisplayed() {
         onView(withId(R.id.text_time)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void currentSatietyIsDisplayed() {
+    public void currentPlayerStatsAreDisplayed() {
         onView(withId(R.id.text_current_satiety)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void currentEnergyIsDisplayed() {
         onView(withId(R.id.text_current_energy)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void currentHealthIsDisplayed() {
         onView(withId(R.id.text_current_health)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void maxSatietyIsDisplayedAs100() {
+    public void maxPlayerStatsAreDisplayedAs100() {
         onView(withId(R.id.text_max_satiety)).check(matches(withText("100")));
-    }
-
-    @Test
-    public void maxEnergyIsDisplayedAs100() {
         onView(withId(R.id.text_max_energy)).check(matches(withText("100")));
-    }
-
-    @Test
-    public void maxHealthIsDisplayedAs100() {
         onView(withId(R.id.text_max_health)).check(matches(withText("100")));
     }
 
     @Test
-    public void inventoryButtonIsDisplayed() {
+    public void clickOnInventoryButtonDisplaysInventoryAsLocation() {
         onView(withId(R.id.button_inventory)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_inventory)).perform(click());
+        onView(withId(R.id.text_location)).check(matches(withText("Inventory")));
+    }
+
+    @Test
+    public void clickOnInventoryButtonDisplaysWeightCapacity() {
+        // testing one distinct feature to make sure InventoryFragment is displayed
+        onView(withId(R.id.button_inventory)).perform(click());
+        onView(withId(R.id.text_max_weight)).check(matches(isDisplayed()));
     }
 }
